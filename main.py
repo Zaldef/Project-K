52a9892d38f14810b4661ed7fe223c9f4de680f6
Zaldef <guilhermeroelli@gmail.com>
2025-04-29 15:10:10 -0300

    Refactors main.py and perfil_paciente.py
    Removes commented-out code in main.py
    Updates UI dimensions in perfil_paciente.ui

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def main():
     inicializar_banco()
 
     app = QApplication(sys.argv)
-    initial_page = Master(1)  # Passando um ID de médico fictício para inicializar a tela principal
-    initial_page.show()
-    #login_window = Login()
-    #login_window.show()
+    #initial_page = Master(1)  # Passando um ID de médico fictício para inicializar a tela principal
+    #initial_page.show()
+    login_window = Login()
+    login_window.show()
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
